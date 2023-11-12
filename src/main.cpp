365801eb9485fefb9066d02cca38151889f6c60c
forest lehrman <forestgithub@skiff.com>
2023-11-12 15:23:17 -0500

    added the creation of a window and fixed the termination

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "hello world!" << std::endl;
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(800, 600, "bruh", nullptr, nullptr);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
 }
