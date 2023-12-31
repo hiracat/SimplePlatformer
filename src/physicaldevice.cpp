01487ae89aee912f64a73b7636e9232b023d1a86
forest lehrman <forestgithub@skiff.com>
2023-12-31 13:05:21 -0500

    added check for if the device supports needed queue families

--- src/physicaldevice.cpp
+++ src/physicaldevice.cpp
@@ -1,10 +1,46 @@
 #include <cstdint>
 #include <map>
+#include <optional>
 #include <stdexcept>
-
 #include <utility>
 #include <vector>
+
 #include <vulkan/vulkan_core.h>
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+
+    bool isComplete() {
+        return graphicsFamily.has_value();
+    }
+};
+
+QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+
+    uint32_t queueFamilyCount{};
+    vkGetPhysicalDeviceQueueFamilyProperties2(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties2> queueFamilies(queueFamilyCount);
+    for (auto& queueFamily : queueFamilies) {
+        queueFamily.sType = VK_STRUCTURE_TYPE_QUEUE_FAMILY_PROPERTIES_2;
+        queueFamily.pNext = nullptr;
+    }
+    vkGetPhysicalDeviceQueueFamilyProperties2(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFamilyProperties.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily = i;
+        }
+        if (indices.isComplete()) {
+            break;
+        }
+        i++;
+    }
+
+    return indices;
+}
 
 uint32_t scorePhysicalDevice(const VkPhysicalDevice& device) {
     VkPhysicalDeviceProperties2 deviceProperties{.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_PROPERTIES_2, .pNext = nullptr};
@@ -12,7 +48,9 @@ uint32_t scorePhysicalDevice(const VkPhysicalDevice& device) {
     vkGetPhysicalDeviceProperties2(device, &deviceProperties);
     vkGetPhysicalDeviceFeatures2(device, &deviceFeatures);
 
-    return 1;
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return indices.isComplete();
 }
 
 void pickPhysicalDevice(VkInstance instance, VkPhysicalDevice physicalDevice) {
