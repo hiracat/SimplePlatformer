063a3ec6c84abfa7c41fe6fe9c10e15487b1a9ba
forest <hiracat@proton.me>
2024-07-18 21:43:12 -0400

    updated packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "vulkan dev environment";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
