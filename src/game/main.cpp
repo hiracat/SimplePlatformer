31c17c08fe9c972dcddb879e2830d56bb8c20695
forest <hiracat@proton.me>
2024-08-16 15:17:38 -0400

    added frametime calculation

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -8,6 +8,7 @@
 #include "../engine/enginedata.h"
 #include "../engine/initengine.h"
 #include "../engine/vulkan/rendering.h"
+#include "../utils/debugprint.h"
 
 int main() {
 
@@ -16,14 +17,19 @@ int main() {
     initEngine(data);
 
     bool windowShouldClose = false;
-    auto startTime         = std::chrono::high_resolution_clock::now();
 
+    auto startTime = std::chrono::high_resolution_clock::now();
+    auto endTime   = std::chrono::high_resolution_clock::now();
+    auto duration  = endTime - startTime;
     while (!glfwWindowShouldClose(data.window.windowPointer)) {
+        startTime = std::chrono::high_resolution_clock::now();
         glfwPollEvents();
-        auto endTime  = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
-        startTime     = endTime;
         drawFrame(data);
+        endTime  = std::chrono::high_resolution_clock::now();
+        duration = endTime - startTime;
     }
+    auto frameTime = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+    debugnote("frametime: " << frameTime);
+
     cleanup(data);
 }
