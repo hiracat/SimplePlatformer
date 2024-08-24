1674f841b19ca87ab55cec806a07c663968f7bd1
forest <hiracat@proton.me>
2024-08-24 16:34:44 -0400

    updated shaders to work with uniform buffers

--- src/game/shaders/vertex.glsl
+++ src/game/shaders/vertex.glsl
@@ -1,17 +1,17 @@
 #version 450
 
-layout(location = 0) in vec2 inPosition;
-layout(location = 1) in vec3 inColor;
 layout(binding = 0) uniform UniformBufferObject {
     mat4 model;
     mat4 view;
     mat4 proj;
 } ubo;
 
+layout(location = 0) in vec2 inPosition;
+layout(location = 1) in vec3 inColor;
+
 layout(location = 0) out vec3 fragColor;
 
-
 void main() {
-    gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 0.0, 1.0); //effectively 2d because the z is set to zero and the in position is a vec2
+    gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 0.0, 1.0);
     fragColor = inColor;
 }
